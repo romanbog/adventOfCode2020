e6fe16bfe775403cb669cba19f5554b379b12a2c
Roman Bogushevich <romanbog78@gmail.com>
2020-12-02 00:21:04 -0800

    cleaned up day2 for readability

--- day2/part2/main.c
+++ day2/part2/main.c
@@ -1,54 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <mcheck.h>
 
 int main(){
 	FILE *fp;
 	fp = fopen("input.txt", "r");
-	//int *array;
 	int total = 0;
-	int tempInt;
-	int fileLineCount = 0;
-	char c;
-	//mtrace();
-/*
-	while(!feof(fp)){
-		c = fgetc(fp);
-		if(c == '\n'){fileLineCount++;}
-	}
-	printf("%d\n", fileLineCount);
-	fclose(fp);
-	fp = fopen("input.txt", "r");
-*/
-	//allocate memory
-	//array = (int*)malloc(fileLineCount * sizeof(int));
-	/*
-	while(fscanf(fp, "%d\n", &tempInt) != EOF){
-		array[i] = tempInt;
-		i++;
-	}
-	*/
 	int min;
 	int max;
 	char toFind;
-	//string pass; 
-	//char pass[20];
 	char pass[1000];
+
 	while(fscanf(fp, "%d-%d %c: %s\n", &min, &max, &toFind, pass)!= EOF){
 		int foundChars = 0;
+		//look at pass[min] and pass[max], determine if they contain the toFind char.
+		//array index starts at 1, so we have to subtract min and max.
 		if(pass[min-1] == toFind){foundChars++;}
 		if(pass[max-1] == toFind){foundChars++;}
+		//increment total only if there is 1 foundChar
 		if(foundChars == 1){total++;}
-
+		
+		//reset string to null
 		for(int i = 0; i < 1000; i++){
 			pass[i] = '\0';
 		}
 	}
-
+	
+	//display result
 	printf("%d\n", total);
-	//free(array);
 
-	//muntrace();
+	fclose(fp);
 	return 0;
 }
