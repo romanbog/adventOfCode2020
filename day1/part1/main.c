05171d4c905dbcbd71765c83594de9d20928bdbe
Roman Bogushevich <romanbog78@gmail.com>
2020-12-01 19:03:44 -0800

    Added dynamic memory management to day1. Cleaned executables from day1 part1 and part2.

--- day1/part1/main.c
+++ day1/part1/main.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#include <mcheck.h> //memory debugging
 
 int main(){
 	FILE *fp;
 	fp = fopen("input.txt", "r");
-	int array[1000] = {0};
+	int *array;
 	int i = 0;
 	int tempInt;
+	int fileLineCount = 0;
+	char c;
+	//mtrace(); //memory debugging
+
+	//count number of lines in input.txt
+	while(!feof(fp)){
+		c = fgetc(fp);
+		if(c == '\n'){fileLineCount++;}
+	}
+	printf("%d\n", fileLineCount);
+	fclose(fp);
+	//re-open input
+	fp = fopen("input.txt", "r");
+
+	array = (int*)malloc(fileLineCount * sizeof(int));
+	//load input into array
 	while(fscanf(fp, "%d\n", &tempInt) != EOF){
 		array[i] = tempInt;
 		i++;
 	}
 
-	for(int j = 0; j < 300; j++){
+	//prints array, for testing
+	/*
+	for(int j = 0; j < fileLineCount; j++){
 		printf("%d\n", array[j]);
 	}
-	
-	for(int j = 0; j < 300; j++){
-		for(int k = j; k < 300; k++){
+	*/
+	for(int j = 0; j < fileLineCount; j++){
+		for(int k = j; k < fileLineCount; k++){
 			if(array[j] + array[k] == 2020){
 				printf("Match Found! %d and %d.\n", array[j], array[k]);
 				printf("Multpilying together... %d\n", array[j] * array[k]);
 			}
 		}
 	}
-	
+	//free allocated memory
+	free(array);
 
+	//muntrace();//memory debugging
+	return 0;
 }
--- day1/part2/main.c
+++ day1/part2/main.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#include <mcheck.h> //memory debugging
 
 int main(){
 	FILE *fp;
 	fp = fopen("input.txt", "r");
-	int array[1000] = {0};
+	int *array;
 	int i = 0;
 	int tempInt;
+	int fileLineCount = 0;
+	char c;
+	//mtrace(); //memory debugging
+
+	//count number of lines in input.txt
+	while(!feof(fp)){
+		c = fgetc(fp);
+		if(c == '\n'){fileLineCount++;}
+	}
+	printf("%d\n", fileLineCount);
+	fclose(fp);
+	//re-open input
+	fp = fopen("input.txt", "r");
+
+	array = (int*)malloc(fileLineCount * sizeof(int));
+	//load input into array
 	while(fscanf(fp, "%d\n", &tempInt) != EOF){
 		array[i] = tempInt;
 		i++;
 	}
 
-	for(int j = 0; j < 300; j++){
+	//prints array, for testing
+	/*
+	for(int j = 0; j < fileLineCount; j++){
 		printf("%d\n", array[j]);
 	}
-	
-	for(int j = 0; j < 300; j++){
-		for(int k = j; k < 300; k++){
-			for(int m = k; m < 300 && m!=0; m++){
+	*/
+	for(int j = 0; j < fileLineCount; j++){
+		for(int k = j; k < fileLineCount; k++){
+			for(int m = k; m < fileLineCount; m++){
 				if(array[j] + array[k] + array[m] == 2020){
-					printf("Match Found! %d, %d and %d\n", array[j], array[k], array[m]);
+					printf("Match Found! %d, %d, and %d.\n", array[j], array[k], array[m]);
 					printf("Multpilying together... %d\n", array[j] * array[k] * array[m]);
 				}
 			}
 		}
 	}
-	
+	//free allocated memory
+	free(array);
 
+	//muntrace();//memory debugging
+	return 0;
 }
