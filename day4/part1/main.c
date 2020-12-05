f69b99a7c5fa31960163515baae8adde8ba2bd38
romanbog <romanbog78@gmail.com>
2020-12-04 19:54:03 -0800

    cleaned up part1, removed executable that shouldn't have been in here

--- day4/part1/main.c
+++ day4/part1/main.c
@@ -6,9 +6,10 @@ int main(){
 	FILE *fp;
 	fp = fopen("input.txt", "r");
 	char inputLine[100];
+	//is our passport valid?
 	int valid = 0;
 	char * token;
-	
+	//no bools lol
 	int byr = 0;
 	int iyr = 0;
 	int eyr = 0;
@@ -19,10 +20,11 @@ int main(){
 	
 	//in each line of the file:
 	while(fgets(inputLine, 100, fp)){
+		//if our first char is a newline, we have a new passport
 		if(inputLine[0] == '\n'){
+			//check if the previous passport was valid
 			if(byr == 1 && iyr == 1 && eyr == 1 && hgt == 1 && hcl == 1 && ecl == 1 && pid == 1){valid++;}
 			//reset all feilds
-			//printf("new pass ============================\n");
 			byr = 0;
 			iyr = 0;
 			eyr = 0;
@@ -32,13 +34,10 @@ int main(){
 			pid = 0;
 		}
 		else{
+			//get a new substring, demilited by spaces
 			token = strtok(inputLine, " ");
 			while(token != NULL){
-				//printf("%s\n", token);
-				//do stuff
-				//char firstThree[3];
-				//memcpy(firstThree, &token[0], 3);
-				//printf("%s\n", firstThree);
+				//check the first char of our substring, and sort our logic from there.
 				switch(token[0]){ //this is a hack
 					case 'b':
 						byr++;
@@ -49,19 +48,25 @@ int main(){
 						iyr++;
 						break;
 					case 'e':
+						//there's two things that start with e.
+						//eyr
 						if(token[1] == 'y'){
 							eyr++;
 							break;
 						}
+						//ecl
 						else{
 							ecl++;
 							break;
 						}
 					case 'h':
+						//two things that start with h
+						//hgt
 						if(token[1] == 'g'){
 							hgt++;
 							break;
 						}
+						//hcl
 						else{
 							hcl++;
 							break;
@@ -73,24 +78,14 @@ int main(){
 						printf("error \n");
 					
 				}
+				//get the next substring from inputLine
+				//this is just how strtok works
 				token = strtok(NULL, " ");
 			}
 		}
-		//for(int i; i < 100; i++){
-			//inputLine[i] = '\0';
-		//}
         }
 	//check last passport lol
-			if(byr == 1 && iyr == 1 && eyr == 1 && hgt == 1 && hcl == 1 && ecl == 1 && pid == 1){valid++;}
-			//reset all feilds
-			//printf("new pass ============================\n");
-			byr = 0;
-			iyr = 0;
-			eyr = 0;
-			hgt = 0;
-			hcl = 0;
-			ecl = 0;
-			pid = 0;
+	if(byr == 1 && iyr == 1 && eyr == 1 && hgt == 1 && hcl == 1 && ecl == 1 && pid == 1){valid++;}
 		
 	printf("valid passports: %d\n", valid);
 
