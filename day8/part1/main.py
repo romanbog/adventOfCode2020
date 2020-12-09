ed3cbe4702dfe4b4f756af9d8f13af99176d2d0a
Roman Bogushevich <romanbog78@gmail.com>
2020-12-08 17:08:28 -0800

    Refactored and commented day8 part1 and day8 part2

--- day8/part1/main.py
+++ day8/part1/main.py
@@ -2,25 +2,30 @@ with open("input.txt", "r") as f:
  array = f.read().split('\n')
 
 accumulator = 0
-#for every group
+#where we're located in our array.
 position = 0
 opcode = array[position].split()
-while opcode[0]:
+while len(array) > position:
+ #split our instruction into a list of strings
  opcode = array[position].split()
+ #If the first element of our instruction is "visited", print accumulator and quit.
  if opcode[0] == "visited":
   print(accumulator)
   quit()
+ #If we have an 'acc', update acumulator and go forward one
  elif opcode[0] == 'acc':
-  #print(array[position])
   accumulator += int(opcode[1])
+  #append "visited" to mark that our string has been visited
   array[position] = "visited " + array[position]
   position += 1
+ #If we have 'jmp', update position based on jmp's value.
  elif opcode[0] == 'jmp' :
-  #print(array[position])
+  #append "visited" to mark that our string has been visited
   array[position] = "visited " + array[position]
   position += int(opcode[1])
+ #If we have an 'nop', just move forward once.
  elif opcode[0] == 'nop':
-  #print(array[position])
+  #append "visited" to mark that our string has been visited
   array[position] = "visited " + array[position]
   position += 1
 print(accumulator)
--- day8/part2/main.py
+++ day8/part2/main.py
@@ -2,13 +2,13 @@ import copy
 
 def noMoreSplits(accumulator, position, array):
  opcode = array[position].split()
+ #Basically, we go through the array of instructions and quit if we run into a loop.
  while len(array) > position:
   opcode = array[position].split()
   if opcode[0] == "visited":
-   #print("quit in nosplits")
+   #If we have a loop, get out of the function asap! 
    return()
   elif opcode[0] == 'acc':
-   #print(array[position])
    accumulator += int(opcode[1])
    array[position] = "visited " + array[position]
    position += 1
@@ -16,19 +16,19 @@ def noMoreSplits(accumulator, position, array):
     print(accumulator)
     quit
   elif opcode[0] == 'jmp' :
-   #print(array[position])
    array[position] = "visited " + array[position]
    position += int(opcode[1])
    if len(array) < position:
     print(accumulator)
     quit
   elif opcode[0] == 'nop':
-   #print(array[position])
    array[position] = "visited " + array[position]
    position += 1
    if len(array) < position:
     print(accumulator)
     quit
+ #If we get out, that means that we've gotten through instructions without any loops! 
+ #Print accumulator ^^;
  print(accumulator)
 
 
@@ -36,38 +36,41 @@ def noMoreSplits(accumulator, position, array):
 with open("input.txt", "r") as f: 
  array = f.read().split('\n')
 
+#This removes an empty element at the back of the array.
 array.pop()
-#print(array)
 accumulator = 0
 position = 0
 opcode = array[position].split()
-while opcode[0]:
+#while opcode[0]:
+while len(array) > position:
+ #Split our line into a list of strings
  opcode = array[position].split()
+ #If we've visited, quit.
  if opcode[0] == "visited":
   quit()
+ #Accumulate. Nothing too special
  elif opcode[0] == 'acc':
-  #print(array[position])
   accumulator += int(opcode[1])
   array[position] = "visited " + array[position]
   position += 1
+ #jmp. This gets interesting!
  elif opcode[0] == 'jmp' :
-  #print(array[position])
-  #print(opcode)
+  #add "visited" to this element in the array
   array[position] = "visited " + array[position]
-  #positionNoSplit = position + 1
-  #arrayNoSplit = array
-  #accumulatorNoSplit = accumulator
+  #make a positionNoSplit to pass into our function that will run with a modified command
+  positionNoSplit = position + 1
+  #Same thing, but with accumulator.
+  accumulatorNoSplit = accumulator
+  #IMPORTANT: we need to DEEP COPY our array. This gives noMoreSplits it's own memory, with its own array
   arrayNoSplit = copy.deepcopy(array)
-  accumulatorNoSplit = copy.deepcopy(accumulator)
-  positionNoSplit = copy.deepcopy(position) + 1
+  #Call noMoreSplits, which will run as if current command was a 'nop'.
   noMoreSplits(accumulatorNoSplit, positionNoSplit, arrayNoSplit)
-  #accumulatorYesSplit = accumulator
-  #positionYesSplit = position + int(opcode[1])
-  #arrayYesSplit = array
-  #canStillSplit(accumulatorYesSplit, positionYesSplit, arrayYesSplit)
+  #increment our own position and keep running.
   position += int(opcode[1])
+ #'nop'. Again, nothing special here.
  elif opcode[0] == 'nop':
-  #print(array[position])
   array[position] = "visited " + array[position]
   position += 1
+#if we've gotten out of the while loop, that means the opcode had no cycles! 
+#We get to print our accumulator c:
 print(accumulator)
