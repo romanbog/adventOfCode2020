fcda1a66bae520094ccd1ab1a8a1303cfddfc531
Roman Bogushevich <romanbog78@gmail.com>
2020-12-09 20:11:39 -0800

    refactored day9.

--- day9/part1/main.py
+++ day9/part1/main.py
@@ -1,27 +1,30 @@
 with open("input.txt", "r") as f: 
  array = f.read().split('\n')
 
+#first 25 are preamble, so we'll start there. 
 position = 25
+#remove last position, it's a whitespace for some reason
+array.pop()
+#while we're in our array
 while len(array) > position:
  toCompare = int(array[position])
  foundMatch = 0
- #print(array[:position])
- #print("searching for " + array[position])
+ #For every element from position to pos-25:
  for element in array[position-25:position]:
   startSpot = array.index(element)
+  #for every element in position to pos-25
   for testElement in array[startSpot+1:position]:
-   #print("Adding :" + element + "+" + testElement)
+   #add together our two elements
    if int(element) + int(testElement) == toCompare:
-    #print("Found!")
+    #if we find a match, we good
     foundMatch = 1
     break
   if foundMatch == 1:
+  #match found, we don't have to look through the rest of our values.
     break
  if foundMatch == 0:
+  #if we don't find a match, print and quit.
   print(array[position])
- #print(array[:position])
+  quit()
   
  position += 1
- #if foundMatch == 0:
-  #print array[position]
-  #break
--- day9/part2/main.py
+++ day9/part2/main.py
@@ -2,17 +2,14 @@ with open("input.txt", "r") as f:
  array = f.read().split('\n')
 
 position = 25
+#same stuff from part1, to get our result.
 while len(array) > position:
  toCompare = int(array[position])
  foundMatch = 0
- #print(array[:position])
- #print("searching for " + array[position])
  for element in array[position-25:position]:
   startSpot = array.index(element)
   for testElement in array[startSpot+1:position]:
-   #print("Adding :" + element + "+" + testElement)
    if int(element) + int(testElement) == toCompare:
-    #print("Found!")
     foundMatch = 1
     break
   if foundMatch == 1:
@@ -20,34 +17,38 @@ while len(array) > position:
  if foundMatch == 0:
   print(array[position])
   break
- #print(array[:position])
   
  position += 1
- #if foundMatch == 0:
-  #print array[position]
-  #break
+#save our result in a result integer.
 result = int(array[position])
+#set runningTotal to the position of array[0]
 runningTotal = int(array[0])
 position = 0
 front = 0
 back = 0
 while runningTotal != result:
- #print(runningTotal, result)
+ #If our runningTotal is less than result, move our back +1, 
+ #and add the value of that index to our runningTotal.
  if runningTotal < result:
   back += 1
   runningTotal += int(array[back])
+ #Else, move our front forward one, and subtract the value that we 
+ #just got rid of.
  elif runningTotal > result:
   runningTotal -= int(array[front])
   front += 1
+ #If it runningTotal and result equal each other, we've found our answer!
  elif runningTotal == result:
-  print(front + back)
   break
 
 newList = []
+#We have to convert our array into an int, starting from front and ending
+#at back. This is so we can call min and max ^^
 for element in array[front:back]:
  newList.append(int(element))
 
+#find min and max inside of our list
 smallest = min(newList)
 largest = max(newList)
+#Print output! :)
 print(smallest + largest)
-#print(int(array[front]) + int(array[back]))
